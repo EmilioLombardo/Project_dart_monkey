c8124aaf55f0386d357f18203d87ef1aeeebde72
EmilioLombardo <emilomb3@gmail.com>
2024-03-01 18:29:46 +0100

    Definer standardverdi for userInput

--- my_cli_app/lib/my_cli_app.dart
+++ my_cli_app/lib/my_cli_app.dart
@@ -6,9 +6,9 @@ int getIntFromUser({prompt="Type an integer: "}) {
     int i = 0;
     while (!valid && i++ < 25) { 
         stdout.write(prompt);
-        String? userInput = stdin.readLineSync();
+        String userInput = stdin.readLineSync() ?? "";
         try {
-            number = int.parse(userInput ?? "");
+            number = int.parse(userInput);
             valid = true;
         } on FormatException {
             print("Invalid input.");
