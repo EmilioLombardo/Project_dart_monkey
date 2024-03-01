eed8b28121993b6b67c62785e144cc81f60e58a3
EmilioLombardo <emilomb3@gmail.com>
2024-03-01 18:27:47 +0100

    Lag getIntFromUser-funksjon med feilbehandling

--- my_cli_app/bin/my_cli_app.dart
+++ my_cli_app/bin/my_cli_app.dart
@@ -1,5 +1,6 @@
 import 'package:my_cli_app/my_cli_app.dart' as my_cli_app;
 
 void main(List<String> arguments) {
-  print('Hello world: ${my_cli_app.calculate()}!');
+  int num = my_cli_app.getIntFromUser();
+  print('You typed: $num');
 }
--- my_cli_app/lib/my_cli_app.dart
+++ my_cli_app/lib/my_cli_app.dart
@@ -1,3 +1,18 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+int getIntFromUser({prompt="Type an integer: "}) {
+    int number = 0;
+    bool valid = false;
+    int i = 0;
+    while (!valid && i++ < 25) { 
+        stdout.write(prompt);
+        String? userInput = stdin.readLineSync();
+        try {
+            number = int.parse(userInput ?? "");
+            valid = true;
+        } on FormatException {
+            print("Invalid input.");
+        }
+    }
+    return number;
 }
