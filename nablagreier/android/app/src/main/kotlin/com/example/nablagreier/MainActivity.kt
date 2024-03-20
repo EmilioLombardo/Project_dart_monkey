19f8144acb1ee440fa06c9fe270abba1c5411a1f
earti <mahaugv@stud.ntnu.no>
2024-03-20 13:25:59 +0100

    Lag første utkast

---
+++ nablagreier/android/app/src/main/kotlin/com/example/nablagreier/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nablagreier
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ nablagreier/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyWebPage());
+}
+
+class MyWebPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            // This is the top image that will scroll away.
+            SliverToBoxAdapter(
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.66, // 2/3 of the screen height
+                decoration: BoxDecoration(
+                  color: Colors.blue, // Background color of the top part
+                  image: DecorationImage(
+                    image: AssetImage('path/to/your/image.jpg'), // Background image
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            // Sticky header that remains visible as you scroll
+            SliverPersistentHeader(
+              delegate: _StickyHeaderDelegate(
+                minHeight: 60.0,
+                maxHeight: 60.0,
+                child: Container(
+                  color: Colors.grey[200], // Background color of the header
+                  child: Center(
+                    child: Text(
+                      'Sticky Header',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              pinned: true,
+            ),
+            // Your main content goes here, below the sticky header
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => ListTile(
+                  title: Text('Item #$index'),
+                ),
+                childCount: 30, // Number of list items
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _StickyHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  _StickyHeaderDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_StickyHeaderDelegate oldDelegate) {
+    return minHeight != oldDelegate.minHeight ||
+           maxHeight != oldDelegate.maxHeight ||
+           child != oldDelegate.child;
+  }
+}
