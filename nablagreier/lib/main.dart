047b2a56d620171fd9eef5c38f97e34170d72248
earti <mahaugv@stud.ntnu.no>
2024-03-22 17:25:35 +0100

    Lag ny fil for toppen og sett størrelser basert på bredde

--- nablagreier/lib/main.dart
+++ nablagreier/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'top_image_section.dart'; 
+
 
 void main() {
   runApp(MyWebPage());
@@ -13,75 +15,7 @@ class MyWebPage extends StatelessWidget {
           slivers: <Widget>[
             // This is the top image that will scroll away.
             SliverToBoxAdapter(
-              child: Container(
-                height: MediaQuery.of(context).size.width * 1 / 3, // 2/5 of the screen width
-                decoration: const BoxDecoration(
-                  color: Color(0xFF061025), // Background color of the top part
-                  image: DecorationImage(
-                    image: AssetImage('images/nablabakgrunn.jpg'), // Background image
-                    fit: BoxFit.cover,
-                    opacity: 0.7,
-                  ),
-                ),
-                child: Stack(
-                  children: <Widget>[
-                    // Title with Custom Font
-                    // ignore: prefer_const_constructors
-                    Positioned(
-                      top: 120, // Adjust the position based on your needs
-                      left: 0,
-                      right: 0,
-                      child: Center(
-                        child: Column(
-                          children: <Widget>[
-                            Text(
-                              'Nabla',
-                              style: TextStyle(
-                                fontFamily: 'Satoshi', // Use your custom font
-                                fontSize: 64,
-                                fontWeight: FontWeight.w700,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Text(
-                              'Linjeforeningen for fysikk og matematikk',
-                              style: TextStyle(
-                                fontFamily: 'Satoshi', // Use your custom font
-                                fontSize: 28,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    // Login Feature (Sign up and Sign in buttons)
-                    Positioned(
-                      bottom: 20, // Adjust the position based on your needs
-                      left: 0,
-                      right: 0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          ElevatedButton(
-                            onPressed: () {
-                              // Handle Sign Up
-                            },
-                            child: Text('Sign Up'),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              // Handle Sign In
-                            },
-                            child: Text('Sign In'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              child: TopImageSection(),
             ),
             // Sticky header that remains visible as you scroll
             SliverPersistentHeader(
@@ -89,11 +23,15 @@ class MyWebPage extends StatelessWidget {
                 minHeight: 60.0,
                 maxHeight: 60.0,
                 child: Container(
-                  color: Color(0xFF1045A6), // Background color of the header
+    
+                  color: Color(0xFF1045A6), // Background color of the header              
                   child: Center(
                     child: Text(
                       'Sticky Header',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
@@ -109,6 +47,7 @@ class MyWebPage extends StatelessWidget {
                 childCount: 30, // Number of list items
               ),
             ),
+
           ],
         ),
       ),
