6c8279e349e1adc01f856e8cf9d593306060469b
earti <mahaugv@stud.ntnu.no>
2024-03-23 12:09:50 +0100

    Lag ikoner i header

--- nablagreier/lib/main.dart
+++ nablagreier/lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'theme_provider.dart';
 import 'top_image_section.dart'; 
 import 'sticky_header.dart';
 
+
 void main() {
-  runApp(MyWebPage());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MyWebPage(),
+    ),
+  );
 }
 
 class MyWebPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
+      theme: ThemeData.light(), // Define light theme
+      darkTheme: ThemeData.dark(), // Define dark theme
+      themeMode: themeProvider.themeMode, // Use the theme from the provider
       home: Scaffold(
         body: CustomScrollView(
           slivers: <Widget>[
--- nablagreier/lib/sticky_header.dart
+++ nablagreier/lib/sticky_header.dart
@@ -1,22 +1,60 @@
-// sticky_header.dart
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'theme_provider.dart';
 
 class StickyHeader extends StatelessWidget {
   const StickyHeader({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context); // Access the ThemeProvider
     return SliverPersistentHeader(
       delegate: _StickyHeaderDelegate(
         minHeight: 60.0,
         maxHeight: 60.0,
         child: Container(
-          color: Color(0xFF1045A6), // Background color of the header
-          child: Center(
-            child: Text(
-              'Sticky Header',
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ),
+          color: const Color(0xFF1045A6), // Background color of the header
+          padding: const EdgeInsets.only(right: 16.0), // Add some padding on the right
+          alignment: Alignment.centerRight, // Align the column to the right
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end, // Align text to the right
+            children: [
+              const Text(
+                'Om Nabla',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              const SizedBox(width: 30), // Spacing between text widgets
+              const Text(
+                'Arrangementer',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              const SizedBox(width: 30), // Spacing between text widgets
+              const Text(
+                'For bedrifter',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              const SizedBox(width: 30), // Spacing between text widgets
+              const Text(
+                'Ny student?',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              const SizedBox(width: 30), // Spacing between text widgets
+              IconButton(
+                icon: Icon(themeProvider.isDarkMode ? Icons.light_mode : Icons.dark_mode),
+                color: Colors.white,
+                onPressed: () {
+                  themeProvider.toggleTheme(!themeProvider.isDarkMode);
+                },
+              ),
+              const SizedBox(width: 15), // Spacing between text widgets
+              InkWell(
+                onTap: () {
+                  // Handle the profile icon tap
+                  
+                },
+                child: const Icon(Icons.account_circle, size: 30.0, color: Colors.white), // Profile icon
+              ),
+            ],
           ),
         ),
       ),
